88e163d4ba61a2fb8f215a04bd00a77a176227a5
Lojze Žust <lojze.zust@gmail.com>
2021-04-20 17:45:20 +0200

    Initialize with latest run

--- js/app.js
+++ js/app.js
@@ -1,41 +1,69 @@
 
-function get_dates(){
+window.onload = function(){
+  // Store elements
+  var plot = document.getElementById('plot');
+  var run_select = document.getElementById('run_select');
+
+  run_select.addEventListener("change", selectDate);
+
+  // Populate date selection
+  getDates()
+  .then(dates => {
+    dates.sort();
+    for(date of dates) {
+      let opt = document.createElement("option");
+      opt.value = date;
+      opt.innerHTML = date;
+      run_select.appendChild(opt);
+    }
+    
+    // Display the most recent run
+    let last_date = dates[dates.length - 1];
+    run_select.value = last_date;
+    displayPlot(last_date);
+  });
+}
+
+function getDates(){
   return fetch('https://gea.arso.gov.si/vg2020-dev/hidra/listHIDRAjson')
   .then(response => response.json())
   .then(data => Promise.resolve(data.Dates));
 }
 
-function get_run(date){
+function getRun(date){
   return fetch('https://gea.arso.gov.si/vg2020-dev/hidra/showHIDRAjson?date='+date)
   .then(response => response.json());
 }
 
-function parse_date(date){
+function parseDate(date){
   return moment(date, "DD.MM.YYYY hh:mm").format();
 }
 
+function selectDate(e){
+  displayPlot(run_select.value);
+}
 
-get_dates()
-.then(dates => get_run(dates[0]))
-.then(data => {
-  console.log(data);
+function displayPlot(date){
+  getRun(date)
+  .then(data => {
+    console.log(data);
 
-  let dates = data.Dates.map(val => parse_date(val));
-  let values = data.Hidra[0].values;
-  let plot = document.getElementById('plot');
+    let dates = data.Dates.map(val => parseDate(val));
+    let values = data.Hidra[42].values;
 
-  let ssh_dates = data.Koper.Dates.map(val => parse_date(val));
-  let ssh_values = data.Koper.values;
+    let ssh_dates = data.Koper.Dates.map(val => parseDate(val));
+    let ssh_values = data.Koper.values;
 
-	Plotly.newPlot(plot, [
-    {
-      x: dates,
-      y: values,
-      name: "HIDRA napoved"
-    },
-    {
-      x:ssh_dates,
-      y:ssh_values,
-      name: "Izmerjena višina"
-    }], { } );
-})
+    Plotly.newPlot(plot, [
+      {
+        x: dates,
+        y: values,
+        name: "HIDRA napoved"
+      },
+      {
+        x:ssh_dates,
+        y:ssh_values,
+        name: "Izmerjena višina"
+      }], { } );
+  });
+}
